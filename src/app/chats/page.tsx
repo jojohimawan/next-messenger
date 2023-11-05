ced31ecf1a06a0c40e3e77c23b386fb2e36e087f
jojohimawan <jordan.hiphop21@gmail.com>
2023-11-05 21:58:51 +0700

    chore: add nextui, create landing page

---
+++ src/app/chats/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from "react";
+
+export default function App() {
+
+
+    return (
+        <>
+        <p>Hello world</p>
+        </>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Providers } from './providers'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,8 +13,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, } : { children: React.ReactNode }) {
 
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='dark'>
+      <body className={inter.className}>
+        <Providers>
+          <div className='bg-black'>
+            {children}
+          </div>
+        </Providers>
+      </body>
     </html>
   )
 }
